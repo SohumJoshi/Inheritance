f28ceac803a6d5617ec99a28989635c39ac311ee
SohumJoshi <74275596+SohumJoshi@users.noreply.github.com>
2021-01-19 20:10:03 -0500

    Add files via upload

---
+++ baseClass.js
@@ -0,0 +1,37 @@
+class BaseClass {
+    
+    constructor(x,y,width,height,angle){
+
+  var options = {
+
+     'restitution': 0.5,
+     'friction':0.8,
+     'density':1.0
+
+  }
+  this.body = Bodies.rectangle(x,y,width,height,options)
+  this.image = loadImage("sprites/base.png");
+  this.width = width
+  this.height = height
+
+World.add(myWorld,this.body)
+
+
+    }
+
+    display() {
+
+var angle = this.body.angle;
+
+push();
+
+translate(this.body.position.x,this.body.position.y);
+rotate(angle)
+imageMode(CENTER)
+fill (255)
+image(this.image,0,0,this.width,this.height)
+pop ();
+
+
+    }
+};
